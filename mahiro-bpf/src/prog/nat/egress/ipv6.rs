d36d2e77923fc44915bfd842c1b83c27295ba747
Sherlock Holo <sherlockya@gmail.com>
2023-04-23 19:01:10 +0800

    feat(mahiro-bpf): add egress/ipv6 mod

--- mahiro-bpf/src/prog/nat/egress/ipv6.rs
+++ mahiro-bpf/src/prog/nat/egress/ipv6.rs
@@ -6,45 +6,45 @@ use aya_bpf::maps::{Array, HashMap, LpmTrie};
 use aya_bpf::programs::TcContext;
 use aya_log_ebpf::{debug, error, warn};
 use network_types::eth::EthHdr;
-use network_types::ip::{IpProto, Ipv4Hdr};
+use network_types::ip::{IpProto, Ipv6Hdr};
 use network_types::tcp::TcpHdr;
 use network_types::udp::UdpHdr;
 
-use crate::conntrack::ipv4::{ConntrackEntry, ConntrackKey, ConntrackPair};
-use crate::conntrack::{ipv4 as ipv4_conntrack, ConntrackType};
+use crate::conntrack::ipv6::{self as ipv6_conntrack, ConntrackEntry, ConntrackKey, ConntrackPair};
+use crate::conntrack::ConntrackType;
 use crate::context_ext::ContextExt;
-use crate::ip_addr::Ipv4Addr;
-use crate::nat::{ipv4, L4Hdr};
+use crate::ip_addr::Ipv6Addr;
+use crate::nat::{ipv6, L4Hdr};
 
 use super::{MAX_LOCAL_IP_RULE_SIZE, NIC_IP_MAP_SIZE};
 
 #[map]
-static NIC_IPV4_MAP: HashMap<u32, Ipv4Addr> = HashMap::with_max_entries(NIC_IP_MAP_SIZE, 0);
+static NIC_IPV6_MAP: HashMap<u32, Ipv6Addr> = HashMap::with_max_entries(NIC_IP_MAP_SIZE, 0);
 
-/// value 1 means is a mahiro network ip
+/// value 1 means is a mahiro ip
 #[map]
-static IPV4_MAHIRO_IP: LpmTrie<Ipv4Addr, u8> =
+static IPV6_MAHIRO_IP: LpmTrie<Ipv6Addr, u8> =
     LpmTrie::with_max_entries(MAX_LOCAL_IP_RULE_SIZE, BPF_F_NO_PREALLOC);
 
-/// mahiro network ipv4 prefix
+/// mahiro network ipv6 prefix
 #[map]
-static IPV4_MAHIRO_PREFIX: Array<u8> = Array::with_max_entries(1, 0);
+static IPV6_MAHIRO_PREFIX: Array<u8> = Array::with_max_entries(1, 0);
 
-pub fn ipv4_egress(ctx: &TcContext, _eth_hdr: &mut EthHdr) -> Result<i32, ()> {
-    let ipv4_hdr = ctx.load_ptr::<Ipv4Hdr>(EthHdr::LEN).ok_or(())?;
-    let src_addr = Ipv4Addr::from(ipv4_hdr.src_addr);
-    let key = Key::new(32, src_addr);
+pub fn ipv6_egress(ctx: &TcContext, _eth_hdr: &mut EthHdr) -> Result<i32, ()> {
+    let ipv6_hdr = ctx.load_ptr::<Ipv6Hdr>(EthHdr::LEN).ok_or(())?;
+    let src_addr = Ipv6Addr::from(unsafe { ipv6_hdr.src_addr.in6_u.u6_addr8 });
+    let key = Key::new(128, src_addr);
 
     // the src ipv4 is not in mahiro network
-    if IPV4_MAHIRO_IP.get(&key).copied().unwrap_or(0) == 0 {
+    if IPV6_MAHIRO_IP.get(&key).copied().unwrap_or(0) == 0 {
         return Ok(TC_ACT_OK);
     }
 
     let egress_nic_index = unsafe { (*ctx.skb.skb).ifindex };
     let nic_ip = unsafe {
-        match NIC_IPV4_MAP.get(&egress_nic_index).copied() {
+        match NIC_IPV6_MAP.get(&egress_nic_index).copied() {
             None => {
-                error!(ctx, "egress nic index {} has no ipv4", egress_nic_index);
+                error!(ctx, "egress nic index {} has no ipv6", egress_nic_index);
 
                 return Ok(TC_ACT_SHOT);
             }
@@ -52,26 +52,26 @@ pub fn ipv4_egress(ctx: &TcContext, _eth_hdr: &mut EthHdr) -> Result<i32, ()> {
         }
     };
 
-    match ipv4_hdr.proto {
-        IpProto::Tcp => ipv4_tcp_egress(ctx, ipv4_hdr, nic_ip),
-        IpProto::Udp => ipv4_udp_egress(ctx, ipv4_hdr, nic_ip),
+    match ipv6_hdr.next_hdr {
+        IpProto::Tcp => ipv6_tcp_egress(ctx, ipv6_hdr, nic_ip),
+        IpProto::Udp => ipv6_udp_egress(ctx, ipv6_hdr, nic_ip),
 
         _ => Ok(TC_ACT_OK),
     }
 }
 
-fn ipv4_tcp_egress(ctx: &TcContext, ipv4_hdr: &mut Ipv4Hdr, nic_ip: Ipv4Addr) -> Result<i32, ()> {
+fn ipv6_tcp_egress(ctx: &TcContext, ipv6_hdr: &mut Ipv6Hdr, nic_ip: Ipv6Addr) -> Result<i32, ()> {
     let tcp_hdr = ctx
-        .load_ptr::<TcpHdr>(EthHdr::LEN + Ipv4Hdr::LEN)
+        .load_ptr::<TcpHdr>(EthHdr::LEN + Ipv6Hdr::LEN)
         .ok_or(())?;
 
-    let src_addr = Ipv4Addr::from(ipv4_hdr.src_addr);
-    let dst_addr = Ipv4Addr::from(ipv4_hdr.dst_addr);
+    let src_addr = Ipv6Addr::from(unsafe { ipv6_hdr.src_addr.in6_u.u6_addr8 });
+    let dst_addr = Ipv6Addr::from(unsafe { ipv6_hdr.dst_addr.in6_u.u6_addr8 });
     let src_port = tcp_hdr.source;
     let dst_port = tcp_hdr.dest;
 
     let snat_key = ConntrackKey::new(src_addr, dst_addr, src_port, dst_port);
-    match ipv4_conntrack::get_conntrack_entry(&snat_key, ConntrackType::Snat) {
+    match ipv6_conntrack::get_conntrack_entry(&snat_key, ConntrackType::Snat) {
         None => {
             // tcp packet is not SYN
             if tcp_hdr.syn() == 0 || tcp_hdr.ack() > 0 {
@@ -85,8 +85,8 @@ fn ipv4_tcp_egress(ctx: &TcContext, ipv4_hdr: &mut Ipv4Hdr, nic_ip: Ipv4Addr) ->
             let dnat_entry = ConntrackEntry::new(dst_addr, src_addr, dst_port, src_port);
             let pair = ConntrackPair::new(&snat_key, &snat_entry, &dnat_key, &dnat_entry);
 
-            if ipv4_conntrack::insert_conntrack_pair(pair).is_err() {
-                error!(ctx, "ipv4 tcp egress insert conntrack failed");
+            if ipv6_conntrack::insert_conntrack_pair(pair).is_err() {
+                error!(ctx, "ipv6 tcp egress insert conntrack failed");
 
                 return Ok(TC_ACT_SHOT);
             }
@@ -98,16 +98,16 @@ fn ipv4_tcp_egress(ctx: &TcContext, ipv4_hdr: &mut Ipv4Hdr, nic_ip: Ipv4Addr) ->
 
             let dnat_key = ConntrackKey::new(dst_addr, nic_ip, dst_port, src_port);
 
-            match ipv4_conntrack::get_conntrack_entry(&dnat_key, ConntrackType::Dnat) {
+            match ipv6_conntrack::get_conntrack_entry(&dnat_key, ConntrackType::Dnat) {
                 None => {
-                    warn!(ctx, "ipv4 tcp egress conntrack dnat miss, need rebuild");
+                    warn!(ctx, "ipv6 tcp egress conntrack dnat miss, need rebuild");
 
                     let dnat_entry = ConntrackEntry::new(dst_addr, src_addr, dst_port, src_port);
 
-                    if ipv4_conntrack::insert_conntrack(&dnat_key, &dnat_entry, ConntrackType::Dnat)
+                    if ipv6_conntrack::insert_conntrack(&dnat_key, &dnat_entry, ConntrackType::Dnat)
                         .is_err()
                     {
-                        error!(ctx, "ipv4 tcp egress insert conntrack dnat failed");
+                        error!(ctx, "ipv6 tcp egress insert conntrack dnat failed");
 
                         return Ok(TC_ACT_SHOT);
                     }
@@ -120,33 +120,33 @@ fn ipv4_tcp_egress(ctx: &TcContext, ipv4_hdr: &mut Ipv4Hdr, nic_ip: Ipv4Addr) ->
         }
     }
 
-    if ipv4::ipv4_snat(ctx, ipv4_hdr, L4Hdr::Tcp(tcp_hdr), Some(nic_ip), None).is_err() {
-        error!(ctx, "ipv4 tcp snat failed");
+    if ipv6::ipv6_snat(ctx, ipv6_hdr, L4Hdr::Tcp(tcp_hdr), Some(nic_ip), None).is_err() {
+        error!(ctx, "ipv6 tcp snat failed");
     }
 
     Ok(TC_ACT_OK)
 }
 
-fn ipv4_udp_egress(ctx: &TcContext, ipv4_hdr: &mut Ipv4Hdr, nic_ip: Ipv4Addr) -> Result<i32, ()> {
+fn ipv6_udp_egress(ctx: &TcContext, ipv6_hdr: &mut Ipv6Hdr, nic_ip: Ipv6Addr) -> Result<i32, ()> {
     let udp_hdr = ctx
-        .load_ptr::<UdpHdr>(EthHdr::LEN + Ipv4Hdr::LEN)
+        .load_ptr::<UdpHdr>(EthHdr::LEN + Ipv6Hdr::LEN)
         .ok_or(())?;
 
-    let src_addr = Ipv4Addr::from(ipv4_hdr.src_addr);
-    let dst_addr = Ipv4Addr::from(ipv4_hdr.dst_addr);
+    let src_addr = Ipv6Addr::from(unsafe { ipv6_hdr.src_addr.in6_u.u6_addr8 });
+    let dst_addr = Ipv6Addr::from(unsafe { ipv6_hdr.dst_addr.in6_u.u6_addr8 });
     let src_port = udp_hdr.source;
     let dst_port = udp_hdr.dest;
 
     let snat_key = ConntrackKey::new(src_addr, dst_addr, src_port, dst_port);
-    match ipv4_conntrack::get_conntrack_entry(&snat_key, ConntrackType::Snat) {
+    match ipv6_conntrack::get_conntrack_entry(&snat_key, ConntrackType::Snat) {
         None => {
             let snat_entry = ConntrackEntry::new(nic_ip, dst_addr, src_port, dst_port);
             let dnat_key = ConntrackKey::new(dst_addr, nic_ip, dst_port, src_port);
             let dnat_entry = ConntrackEntry::new(dst_addr, src_addr, dst_port, src_port);
             let pair = ConntrackPair::new(&snat_key, &snat_entry, &dnat_key, &dnat_entry);
 
-            if ipv4_conntrack::insert_conntrack_pair(pair).is_err() {
-                error!(ctx, "ipv4 udp egress insert conntrack failed");
+            if ipv6_conntrack::insert_conntrack_pair(pair).is_err() {
+                error!(ctx, "ipv6 udp egress insert conntrack failed");
 
                 return Ok(TC_ACT_SHOT);
             }
@@ -158,16 +158,16 @@ fn ipv4_udp_egress(ctx: &TcContext, ipv4_hdr: &mut Ipv4Hdr, nic_ip: Ipv4Addr) ->
 
             let dnat_key = ConntrackKey::new(dst_addr, nic_ip, dst_port, src_port);
 
-            match ipv4_conntrack::get_conntrack_entry(&dnat_key, ConntrackType::Dnat) {
+            match ipv6_conntrack::get_conntrack_entry(&dnat_key, ConntrackType::Dnat) {
                 None => {
-                    warn!(ctx, "ipv4 udp egress conntrack dnat miss, need rebuild");
+                    warn!(ctx, "ipv6 udp egress conntrack dnat miss, need rebuild");
 
                     let dnat_entry = ConntrackEntry::new(dst_addr, src_addr, dst_port, src_port);
 
-                    if ipv4_conntrack::insert_conntrack(&dnat_key, &dnat_entry, ConntrackType::Dnat)
+                    if ipv6_conntrack::insert_conntrack(&dnat_key, &dnat_entry, ConntrackType::Dnat)
                         .is_err()
                     {
-                        error!(ctx, "ipv4 udp egress insert conntrack dnat failed");
+                        error!(ctx, "ipv6 udp egress insert conntrack dnat failed");
 
                         return Ok(TC_ACT_SHOT);
                     }
@@ -180,8 +180,8 @@ fn ipv4_udp_egress(ctx: &TcContext, ipv4_hdr: &mut Ipv4Hdr, nic_ip: Ipv4Addr) ->
         }
     }
 
-    if ipv4::ipv4_snat(ctx, ipv4_hdr, L4Hdr::Udp(udp_hdr), Some(nic_ip), None).is_err() {
-        error!(ctx, "ipv4 udp snat failed");
+    if ipv6::ipv6_snat(ctx, ipv6_hdr, L4Hdr::Udp(udp_hdr), Some(nic_ip), None).is_err() {
+        error!(ctx, "ipv6 udp snat failed");
     }
 
     Ok(TC_ACT_OK)
