63dea4d03dc15f5f2585f0fc1665d01e19f3367f
Sherlock Holo <sherlockya@gmail.com>
2023-05-12 22:55:56 +0800

    fix(mahiro): get_packet_ip ub

--- mahiro/src/ip_packet.rs
+++ mahiro/src/ip_packet.rs
@@ -1,4 +1,5 @@
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::ptr::addr_of;
 
 use network_types::ip::{Ipv4Hdr, Ipv6Hdr};
 
@@ -11,25 +12,36 @@ pub enum IpLocation {
 pub fn get_packet_ip(packet: &[u8], location: IpLocation) -> Option<IpAddr> {
     let ip_ver = packet[0] >> 4;
     if ip_ver == 0b100 {
+        let ipv4_hdr = packet.as_ptr().cast::<Ipv4Hdr>();
         // Safety: ip version is ipv4
-        let ipv4_hdr = unsafe { &*(packet.as_ptr() as *const Ipv4Hdr) };
 
-        match location {
-            IpLocation::Src => Some(IpAddr::V4(Ipv4Addr::from(u32::from_be(ipv4_hdr.src_addr)))),
-            IpLocation::Dst => Some(IpAddr::V4(Ipv4Addr::from(u32::from_be(ipv4_hdr.dst_addr)))),
+        unsafe {
+            match location {
+                IpLocation::Src => Some(IpAddr::V4(Ipv4Addr::from(u32::from_be(
+                    addr_of!((*ipv4_hdr).src_addr).read_unaligned(),
+                )))),
+                IpLocation::Dst => Some(IpAddr::V4(Ipv4Addr::from(u32::from_be(
+                    addr_of!((*ipv4_hdr).dst_addr).read_unaligned(),
+                )))),
+            }
         }
     } else if ip_ver == 0b0110 {
+        let ipv6_hdr = packet.as_ptr().cast::<Ipv6Hdr>();
         // Safety: ip version is ipv6
         unsafe {
-            let ipv6_hdr = &*(packet.as_ptr() as *const Ipv6Hdr);
-
             match location {
-                IpLocation::Src => {
-                    Some(IpAddr::V6(Ipv6Addr::from(ipv6_hdr.src_addr.in6_u.u6_addr8)))
-                }
-                IpLocation::Dst => {
-                    Some(IpAddr::V6(Ipv6Addr::from(ipv6_hdr.dst_addr.in6_u.u6_addr8)))
-                }
+                IpLocation::Src => Some(IpAddr::V6(Ipv6Addr::from(
+                    addr_of!((*ipv6_hdr).src_addr)
+                        .read_unaligned()
+                        .in6_u
+                        .u6_addr8,
+                ))),
+                IpLocation::Dst => Some(IpAddr::V6(Ipv6Addr::from(
+                    addr_of!((*ipv6_hdr).dst_addr)
+                        .read_unaligned()
+                        .in6_u
+                        .u6_addr8,
+                ))),
             }
         }
     } else {
